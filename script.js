ce472c6a848fb064370a2be80c173e0d952ab066
Fernando De Jesus <fdejesus95@outlook.com>
2024-04-03 22:13:01 -0400

    updated

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const h2 = document.createElement("h2");
-document.querySelector("body").appendChild(h2);
+const h1 = document.querySelector("h1");
+
+// document.querySelector("body").appendChild(h1);
+
+h1.addEventListener('mouseover' ,changeColor)
+function changeColor(){
+    h1.style.color = 'green'
+}
+
+h1.addEventListener('mouseout', changeColorBack)
+function changeColorBack(){
+    h1.style.color = 'black'
+}
 
 const dogs = document.querySelectorAll('p')
